1d3b1e31ccb7f3d558db288ddd80500f6c856d73
Vinny Martinez <vinny@vinnymartinez.com>
2017-06-17 11:40:00 -0400

    did it again

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
-# your code goes here
+require 'pry'
+def begins_with_r(array_of_words)
+  !array_of_words.index { |word| word[0].upcase != 'R' }
+end
+
+def contain_a(array_of_words)
+  array_of_words.select { |word| word.upcase.include? 'A' }
+end
+
+def first_wa(array_of_words)
+  array_of_words.find { |word| word.slice(0, 2).upcase == 'WA' }
+end
+
+def remove_non_strings(array)
+  array.select { |word| word.is_a? String }
+end
+
+def count_elements(array)
+  counting_hash = {}
+  array.each { |item|
+    if counting_hash[item[:name]]
+      counting_hash[item[:name]] += 1
+    else
+      counting_hash[item[:name]] = 1
+    end
+  }
+  counting_hash.keys.map { |key|
+    a = {:name => key, :count => counting_hash[key]}
+  }
+end
+
+def merge_data(keys, data)
+  keys.map { |key| key.merge(data[0][key[:first_name]]) }
+end
+
+def find_cool(array)
+  array.select { |data_hash| data_hash[:temperature] == "cool" }
+end
+
+def organize_schools(schools)
+  locations = {}
+  schools.each { |key, value|
+    if !locations[value[:location]]
+      locations[value[:location]] = [key]
+    else
+      locations[value[:location]] << key
+    end
+  }
+  locations
+end
